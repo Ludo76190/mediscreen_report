554210ee76e0899969d5c261400f8f9a895e810b
Ludovic Allegaert <ludovic.allegaert@axa.fr>
2022-02-03 16:56:16 +0100

    initial commit

---
+++ src/main/java/com/ludo/msreport/MsreportApplication.java
@@ -0,0 +1,13 @@
+package com.ludo.msreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsreportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ludo/msreport/MsreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludo.msreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsreportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
